0023754d893c8f1e2e0a98af7f79b85ea01f413e
N0932809 <N0932809@my.ntu.ac.uk>
2020-11-09 19:42:07 +0000

    maxLatitude test file

---
+++ src/gpx-tests/maxLatitude.cpp
@@ -0,0 +1,38 @@
+#include <boost/test/unit_test.hpp>
+
+#include "route.h"
+
+using namespace GPS;
+
+BOOST_AUTO_TEST_SUITE( maxLatitude )
+
+/* Route.maxLatitude() should return the maximum latitude of the route, which is taken from the content of
+ * the <rte pt> element (within the <re> element) in the GPX data that the Route was
+ * constructed from.
+ *
+ * The main testing consideration is whether a latitude value is present or absent in the GPX data.
+ *
+ * We start by testing a typical input: a route containing a single position with just a latitude value
+ *
+ * We next test for absent values.  There are two possibilites: the <rte pt> element may be
+ * missing, or the content of the element may be entirely blank space.
+ *
+ * Finally, we test that blank space before and after the name is dropped, including
+ * the corner case where there is blank space at both the front and the back of the
+ * same name.  We also test that blank space within the name is not dropped.
+ */
+
+const std::string defaultName = "Unnamed Route";
+const bool isFileName = false; // All GPX data in this suite is provided as strings.
+
+// Check the name of a GPX route containing a non-empty <rte pt> element within <rte>.
+BOOST_AUTO_TEST_CASE( position_location_present )
+{
+   const std::string gpxData =
+     "<gpx><rte><name>MyRoute</name><rtept lat=\"0\" lon=\"0\"></rtept></rte></gpx>";
+   Route route = Route(gpxData, isFileName);
+
+   BOOST_CHECK_EQUAL( route.maxLatitude(), "0" );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
